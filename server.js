6aa2b48b99f9acfcaead0d0c14b9fa9bbd0f4643
sbsromero <sebas-romero94@hotmail.com>
2017-03-05 17:16:16 -0500

    Prop repo from heroku

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else{
+    res.redirect('http://'+req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));
-app.listen(3000, function(){
-  console.log("Express server esta corriendo en el puerto 3000");
+app.listen(PORT, function(){
+  console.log('Express server esta corriendo en el puerto '+PORT);
 });
