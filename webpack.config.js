89975d47c60df6867ef5c1b191237e8c2d2aea21
sbsromero <sebas-romero94@hotmail.com>
2017-03-10 17:30:03 -0500

    sass para css

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
       WeatherMessage: 'app/components/WeatherMessage.jsx',
       openWeatherMap: 'app/api/openWeatherMap.jsx',
       ErrorModal: 'app/components/ErrorModal.jsx',
-      applicationStyles: 'app/style/app.css'
+      applicationStyles: 'app/style/app.scss'
     },
     extensions: ['','.js','.jsx']
   },
